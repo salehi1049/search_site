43a89125bd14dbb802cb3df014c0a9cca6f3d3e4
salehi1049 <h.salehi1049@gmail.com>
2022-09-08 17:37:14 +0430

    http is not connect in scrap

--- scrap.py
+++ scrap.py
@@ -1,21 +1,33 @@
 from bs4 import BeautifulSoup
 import requests
+from openpyxl import Workbook
+workbook = Workbook()
+sheet = workbook.active
+article_title=[]
+article_author=[]
 
-page = requests.get("https://sadaf.mrsservers.com:8443/login_up.php?success_redirect_url=%2Fsmb%2Flog-file%2Fbrowser%2Fid%2F769")
+
+page = requests.get("https://sadaf.mrsservers.com:8443/smb/log-file/browser/id/769")
 
 soup = BeautifulSoup(page.content, 'html.parser')
 
-s_id = soup.find_all(class_ ="number")
+s_id2= soup.findAll('div',attrs={'class':'brand-collapsed'})
+s_id1= soup.findAll('tbody.tr.td',attrs={'class':'number state-default'})
+s_id= soup.findAll('td',attrs={'class':'number'})
 
-s_t = soup.find_all(class_="number state-default")
+s_t = soup.findAll('td',attrs={'class':"number state-default"})
 
-tonight = forecast_items[0]
-print(tonight.prettify())
-###############
-period = tonight.find(class_ = "period-name").get_text()
-short_Desc = tonight.find(class_ = "short-desc").get_text()
-temp = tonight.find(class_ = "temp temp-high").get_text()
+for i in range(0,len(soup1)):
+         article_title.append(s_id2[i].text)
+         article_author.append(s_id[i].text)
+print("page{} : ".format(j),article_title)
+print(s_id)
+print(s_t)
 
-print(period)
-print(short_Desc)
-print(temp)
+cnt=0
+for i in range(0,len(article_author)):
+    for j in range(0,len(article_author[i])):
+        cnt+=1
+        sheet["A{}".format(cnt)] = article_title[i][j]
+        sheet["B{}".format(cnt)] = article_author[i][j]
+workbook.save(filename="hello_world.xlsx")
