ca0a27bb72ba3e607108337d2ad765021fc9b0a8
salehi1049 <h.salehi1049@gmail.com>
2022-09-12 22:17:34 +0430

    اضافه کردن از یک سایت دیگه نمی تونم از سایت خودم صفحه وب رو بخونم

--- scrap.py
+++ scrap.py
@@ -12,15 +12,19 @@ article_author=[]
 # tables=pd.read_html("https://sadaf.mrsservers.com:8443/smb/log-file/browser/id/769")
 # df1=tables[0]
 # temp=df1.head()
+page1 = requests.get("https://virgool.io/python-community/%D8%A7%D8%B3%D8%AA%D8%AE%D8%B1%D8%A7%D8%AC-%D8%A7%D8%B7%D9%84%D8%A7%D8%B9%D8%A7%D8%AA-%D8%A7%D8%B2-%D8%B5%D9%81%D8%AD%D8%A7%D8%AA-%D9%88%D8%A8-%D8%A8%D8%A7-python-oztzkyxir0az")
+soup = BeautifulSoup(page1.text, "html.parser")
 
 page = requests.get("https://sadaf.mrsservers.com:8443/smb/log-file/browser/id/769")
-soup = BeautifulSoup(page.content, 'html.parser')
+soup = BeautifulSoup(page.text, "html.parser")
 
-s_id2= soup.findAll('div',attrs={'class':'brand-collapsed'})
-s_id1= soup.findAll('tbody.tr.td',attrs={'class':'number state-default'})
-s_id= soup.findAll('td',attrs={'class':'number'})
+# soup_e= soup.find("tr",attrs={"class":"even"})
+# soup_o= soup.find("tr",attrs={"class":"odd"})
 
-s_t = soup.findAll('td',attrs={'class':"number state-default"})
+s_id1= soup.find_all("td",attrs={"class":"number state-default"})
+s_id = soup.find_all('td',attrs={'class':'number'})
+
+s_t = soup.find_all('td',attrs={'class':"number state-default"})
 
 for i in range(0,len(soup1)):
          article_title.append(s_id2[i].text)
